72788af5aa18fb121054361ca12cf4a2baf3a00d
Dima Boyko <dimatmg@gmail.com>
2024-01-23 17:00:02 +0200

    spoiler v3

--- js.js
+++ js.js
@@ -1,26 +1,48 @@
-var spd_index='';//открыть ппри загрузке страницы
-var template_spd_index='#spd_index=';//шаблон запроса
-$(document).ready(function(){
-  $('.spd-content').css('display','none'); //сначала скрываем все елементы
-  //определяет активный елемент при загрузке
-  spd_index=location.hash;
-  if(spd_index.indexOf(template_spd_index)>-1)//определен активный елемент
-  {
-    spd_index=spd_index.substr(template_spd_index.length,spd_index.length-template_spd_index.length);
-    $('.spoiler-dix:eq('+spd_index+')').children('.spd-content').slideDown();//открывает активный елемент
-  }
-  });
-$(function(){
-  $('.spd-header').click(function(){
-      var activ=$(this).next('div');//выбраный блок
-      var astyle=activ.css('display') ;//скрыть активный елемент
-      $('.spd-content').slideUp() ;//скрыть все елементы
-      if(astyle!='block')activ.slideDown();//отображать только скрытый елемент
-      let  _spoiler=jQuery(this).closest(".spoiler-dix");
-      if(_spoiler.hasClass("spd-active")){
-        _spoiler.removeClass('spd-active');
-      }else{
-        _spoiler.addClass('spd-active');
-      }
+
+class spoiler_dix{
+  constructor(){
+    this.one=true;
+    let t=this;
+    document.addEventListener('DOMContentLoaded', function(){
+      document.querySelectorAll('.spd-header').forEach(function(i){
+        let spoiler=i.closest('.spoiler-dix');
+        spoiler.setAttribute('data-max-height',spoiler.offsetHeight+'px');
+        spoiler.querySelector('.spd-content').style.maxHeight='0px';
+        spoiler.querySelector('.spd-content').style.overflow = "hidden";
+
+        i.addEventListener('click',function(){
+
+          let spoiler=i.closest('.spoiler-dix');
+          if(spoiler.classList.contains("spd-active")){
+            spoiler.classList.remove("spd-active");
+            spoiler.querySelector('.spd-content').style.maxHeight='0px';
+            spoiler.querySelector('.spd-content').style.overflow = "hidden";
+          }else{
+            if(t.one){
+              document.querySelectorAll('.spoiler-dix.spd-active').forEach(function(i){
+                i.classList.remove("spd-active");
+                i.querySelector('.spd-content').style.maxHeight='0px';
+                i.querySelector('.spd-content').style.overflow = "hidden";
+              });
+            }
+            spoiler.classList.add("spd-active");
+            spoiler.querySelector('.spd-content').style.maxHeight=spoiler.getAttribute('data-max-height');
+            setTimeout(()=>{
+              spoiler.querySelector('.spd-content').style.overflow = "visible";
+              spoiler.querySelector('.spd-content').style.removeProperty('max-height');
+            },500);
+          }
+
+        });
+      });
     });
-});
+  }
+
+  OnlyOne(value=true){
+    this.one=value;
+  }
+
+}
+
+var SpoilerDix = new spoiler_dix();
+//SpoilerDix.OnlyOne(false);
