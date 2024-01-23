4fd168a85a583c3b3fdcbfb9e23b5cd57f535a0c
Dima Boyko <dimatmg@gmail.com>
2024-01-23 16:59:05 +0200

    spoiler+ v2.1

--- js.js
+++ js.js
@@ -1,20 +1,26 @@
-var index='';//открыть ппри загрузке страницы
-var template_index='#index=';//шаблон запроса
+var spd_index='';//открыть ппри загрузке страницы
+var template_spd_index='#spd_index=';//шаблон запроса
 $(document).ready(function(){
   $('.spd-content').css('display','none'); //сначала скрываем все елементы
   //определяет активный елемент при загрузке
-  index=location.hash;
-  if(index.indexOf(template_index)>-1)//определен активный елемент
+  spd_index=location.hash;
+  if(spd_index.indexOf(template_spd_index)>-1)//определен активный елемент
   {
-    index=index.substr(template_index.length,index.length-template_index.length);
-    $('.spoiler-dix:eq('+index+')').children('.spd-content').slideDown();//открывает активный елемент
+    spd_index=spd_index.substr(template_spd_index.length,spd_index.length-template_spd_index.length);
+    $('.spoiler-dix:eq('+spd_index+')').children('.spd-content').slideDown();//открывает активный елемент
   }
   });
 $(function(){
   $('.spd-header').click(function(){
-    var activ=$(this).next('div');//выбраный блок
-    var astyle=activ.css('display') ;//скрыть активный елемент
-    $('.spd-content').slideUp() ;//скрыть все елементы
-    if(astyle!='block')activ.slideDown();//отображать только скрытый елемент
+      var activ=$(this).next('div');//выбраный блок
+      var astyle=activ.css('display') ;//скрыть активный елемент
+      $('.spd-content').slideUp() ;//скрыть все елементы
+      if(astyle!='block')activ.slideDown();//отображать только скрытый елемент
+      let  _spoiler=jQuery(this).closest(".spoiler-dix");
+      if(_spoiler.hasClass("spd-active")){
+        _spoiler.removeClass('spd-active');
+      }else{
+        _spoiler.addClass('spd-active');
+      }
     });
 });
--- spoiler_dix.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){$('.spd-content').css('display','none')});
-$(function(){
-  $('.spd-header').click(function(){
-    $('.spd-content').slideUp() ;
-    $(this).next('div').slideDown();
-    });
-});
